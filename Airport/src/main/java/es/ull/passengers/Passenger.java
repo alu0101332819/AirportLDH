a5abc6e5700bb756dc9a7427238dbeb779f387f4
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-11-28 13:15:36 +0000

    Update Passenger.java
    Merge some if statements with the enclosing one to solve a Code Smell.

--- Airport/src/main/java/es/ull/passengers/Passenger.java
+++ Airport/src/main/java/es/ull/passengers/Passenger.java
@@ -61,14 +61,12 @@ public class Passenger {
 
     public void joinFlight(Flight flight) {
         Flight previousFlight = this.flight;
-        if (null != previousFlight) {
-            if (!previousFlight.removePassenger(this)) {
+        if (null != previousFlight && !previousFlight.removePassenger(this)) {
                 throw new RuntimeException("Cannot remove passenger");
             }
         }
         setFlight(flight);
-        if (null != flight) {
-            if (!flight.addPassenger(this)) {
+        if (null != flight && !flight.addPassenger(this)) {
                 throw new RuntimeException("Cannot add passenger");
             }
         }
