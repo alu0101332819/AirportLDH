8cf0f2856c2575cf72ce2ecdcdff636288d9c5d0
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-11-28 12:06:32 +0000

    Update FlightPassengerTest.java

--- Airport/src/test/java/es/ull/flightspassengers/FlightPassengerTest.java
+++ Airport/src/test/java/es/ull/flightspassengers/FlightPassengerTest.java
@@ -1,26 +1,67 @@
 package es.ull.flightspassengers;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import es.ull.flights.Flight;
 import es.ull.passengers.Passenger;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+public class FlightPassengersTest {
 
-class FlightPassengerTest {
+	@DisplayName("Tests intercambio correcto pasajeros")
+	@Nested
+	class InterchangeFlightsPassengers {
 
-    @Test
-    void testFlightNumberValidation() {
-        assertThrows(RuntimeException.class, () -> new Flight("123", 100));
-        assertDoesNotThrow(() -> new Flight("AA1234", 100));
-    }
+	    private Flight vueloFuerteventura;
+	    private Flight vueloLanzarote;
+	    private Passenger jaime;
 
-    @Test
-    void testAddPassenger() {
-        Flight flight = new Flight("AA1234", 1);
-        Passenger passenger1 = new Passenger("ID1", "John Doe", "US");
-        Passenger passenger2 = new Passenger("ID2", "Jane Doe", "US");
+	    @BeforeEach
+	    void setUp() {
+	    	vueloFuerteventura = new Flight("FV001", 100);
+	    	vueloLanzarote = new Flight("LZ001", 82);
+	    	jaime = new Passenger("id-PS01-2","Jaime", "ES");
+	    }
 
-        assertTrue(flight.addPassenger(passenger1));
-        assertThrows(RuntimeException.class, () -> flight.addPassenger(passenger2));
-    }
+	        @Test
+	        @DisplayName("Intercambio correcto entre vuelos a través de joinFlight")
+	        void testMethodInterchangePassenger() {
+
+	        	jaime.joinFlight(vueloLanzarote);
+	        	jaime.joinFlight(vueloFuerteventura);
+
+	            assertAll("Verifica todas las condiciones un intercambio a través de joinFlight",
+	                    () -> assertEquals("FV001", jaime.getFlight().getFlightNumber()),
+	                    () -> assertEquals(1, vueloFuerteventura.getNumberOfPassengers()),
+	                    () -> assertEquals(0, vueloLanzarote.getNumberOfPassengers())
+	            );
+	        }
+
+	        @Test
+	        @DisplayName("Intercambio manual entre vuelos a través de setFlight and removePassenger")
+	        void testManualInterchangeMethod() {
+
+	        	jaime.joinFlight(vueloFuerteventura);
+	        	jaime.setFlight(vueloLanzarote);
+
+	        	assertAll("Intercambio a través de removePassenger y setFlight",
+	        	() -> assertEquals("LZ001", jaime.getFlight().getFlightNumber()),
+	        	() -> assertEquals(1, vueloFuerteventura.getNumberOfPassengers()),
+	        	() -> assertEquals(0, vueloLanzarote.getNumberOfPassengers()),
+	        	() -> {
+	        		vueloLanzarote.addPassenger(jaime);
+	        		assertEquals(1, vueloLanzarote.getNumberOfPassengers());
+	        	},
+	        	() -> assertEquals(1, vueloFuerteventura.getNumberOfPassengers()),
+	        	() -> {
+	        		vueloFuerteventura.removePassenger(jaime);
+	        		assertEquals(0, vueloFuerteventura.getNumberOfPassengers());
+	        	});
+	        }
+	    }
 }
