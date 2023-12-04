6b6df09237d9b682f69118472aa8718facd9dc91
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-04 14:11:11 +0000

    Update PassengersTest.java

--- src/test/java/es/ull/passengers/PassengersTest.java
+++ src/test/java/es/ull/passengers/PassengersTest.java
@@ -1,51 +1,128 @@
 package es.ull.passengers;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import es.ull.flights.Flight;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import es.ull.flights.Flight;
+import java.util.Locale;
 
-public class PassengersTest {
+public class PassengerTest {
 
-    @DisplayName("Tests para pasajeros")
+    @DisplayName("Passenger Tests")
     @Nested
-    class PassengersCommonTests {
+    class PassengerTests {
 
-        private Flight vueloFuerteventura;
-        private Flight vueloLanzarote;
-        private Passenger laura;
-        private Passenger eduardo;
-        private Passenger paula;
+        private Passenger passenger;
+        private Flight flight;
 
         @BeforeEach
         void setUp() {
-            vueloFuerteventura = new Flight("FV001", 100);
-            vueloLanzarote = new Flight("LZ001", 82);
-            laura = new Passenger("id-PS01-1", "Laura", "ES");
-            eduardo = new Passenger("id-PS02-1", "Eduardo", "GE");
-            paula = new Passenger("id-PS02-2", "Paula", "US");
+            passenger = new Passenger("id-123", "John Doe", "US");
+            flight = new Flight("FL001", 150);
         }
 
         @Test
-        @DisplayName("Comprobación de atributos")
-        void testParametersPassenger() {
+        @DisplayName("Check Passenger Information")
+        void testGetters() {
+            assertEquals("id-123", passenger.getIdentifier());
+            assertEquals("John Doe", passenger.getName());
+            assertEquals("US", passenger.getCountryCode());
+            assertNull(passenger.getFlight());
+        }
 
-            laura.setFlight(vueloLanzarote);
+        @Test
+        @DisplayName("Join and Leave Flight")
+        void testJoinAndLeaveFlight() {
+            assertNull(passenger.getFlight());
 
-            assertAll("Verifica todas las condiciones un intercambio a través de joinFlight",
-                    () -> assertEquals("Laura", laura.getName()), // Fix: Corrected name to "Laura"
-                    () -> assertEquals("GE", eduardo.getCountryCode()),
-                    () -> assertEquals(null, paula.getFlight()),
-                    () -> {
-                        paula.setFlight(vueloFuerteventura);
-                        assertEquals("FV001", paula.getFlight().getFlightNumber());
-                    }
-            );
+            passenger.joinFlight(flight);
+
+            assertNotNull(passenger.getFlight());
+            assertEquals("FL001", passenger.getFlight().getFlightNumber());
+            assertEquals(1, flight.getNumberOfPassengers());
+
+            passenger.joinFlight(null);
+
+            assertNull(passenger.getFlight());
+            assertEquals(0, flight.getNumberOfPassengers());
+        }
+
+        @Test
+        @DisplayName("Join Another Flight")
+        void testJoinAnotherFlight() {
+            Flight anotherFlight = new Flight("FL002", 200);
+
+            passenger.joinFlight(flight);
+            assertNotNull(passenger.getFlight());
+            assertEquals(1, flight.getNumberOfPassengers());
+
+            passenger.joinFlight(anotherFlight);
+            assertNotNull(passenger.getFlight());
+            assertEquals("FL002", passenger.getFlight().getFlightNumber());
+            assertEquals(1, anotherFlight.getNumberOfPassengers());
+            assertEquals(0, flight.getNumberOfPassengers());
+        }
+
+        @Test
+        @DisplayName("Invalid Country Code")
+        void testInvalidCountryCode() {
+            assertThrows(RuntimeException.class, () -> new Passenger("id-456", "Jane Doe", "XX"));
+        }
+
+        @Test
+        @DisplayName("Manual Interchange Method")
+        void testManualInterchangeMethod() {
+            Flight anotherFlight = new Flight("FL003", 120);
+
+            passenger.joinFlight(flight);
+            assertNotNull(passenger.getFlight());
+            assertEquals(1, flight.getNumberOfPassengers());
+
+            passenger.setFlight(anotherFlight);
+            assertNotNull(passenger.getFlight());
+            assertEquals("FL003", passenger.getFlight().getFlightNumber());
+            assertEquals(1, anotherFlight.getNumberOfPassengers());
+            assertEquals(0, flight.getNumberOfPassengers());
+        }
+
+        @Test
+        @DisplayName("Remove Passenger from Previous Flight")
+        void testRemovePassengerFromPreviousFlight() {
+            Flight anotherFlight = new Flight("FL004", 100);
+            passenger.joinFlight(flight);
+
+            assertNotNull(passenger.getFlight());
+            assertEquals(1, flight.getNumberOfPassengers());
+
+            passenger.joinFlight(anotherFlight);
+            assertNotNull(passenger.getFlight());
+            assertEquals("FL004", passenger.getFlight().getFlightNumber());
+            assertEquals(1, anotherFlight.getNumberOfPassengers());
+            assertEquals(0, flight.getNumberOfPassengers());
+        }
+
+        @Test
+        @DisplayName("Attempt to Remove Passenger from Previous Flight Fails")
+        void testRemovePassengerFromPreviousFlightFails() {
+            Flight anotherFlight = new Flight("FL005", 80);
+            passenger.joinFlight(flight);
+
+            assertNotNull(passenger.getFlight());
+            assertEquals(1, flight.getNumberOfPassengers());
+
+            // Simulate a failure to remove the passenger from the previous flight
+            flight.setCanRemovePassenger(false);
+
+            assertThrows(RuntimeException.class, () -> passenger.joinFlight(anotherFlight));
+
+            // Ensure that the passenger is still in the initial flight
+            assertNotNull(passenger.getFlight());
+            assertEquals("FL001", passenger.getFlight().getFlightNumber());
+            assertEquals(1, flight.getNumberOfPassengers());
         }
     }
 }
