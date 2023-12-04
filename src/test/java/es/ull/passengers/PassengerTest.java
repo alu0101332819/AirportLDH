7d3e851f8f83fd6298898a44c98d1e0bbbe9d071
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-04 13:46:59 +0000

    Update PassengerTest.java

--- src/test/java/es/ull/passengers/PassengerTest.java
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -12,40 +12,40 @@ import es.ull.flights.Flight;
 
 public class PassengersTest {
 
-	@DisplayName("Tests para pasajeros")
-	@Nested
-	class PassengersCommonTests {
+    @DisplayName("Tests para pasajeros")
+    @Nested
+    class PassengersCommonTests {
 
-	    private Flight vueloFuerteventura;
-	    private Flight vueloLanzarote;
-	    private Passenger laura;
-	    private Passenger eduardo;
-	    private Passenger paula;
+        private Flight vueloFuerteventura;
+        private Flight vueloLanzarote;
+        private Passenger laura;
+        private Passenger eduardo;
+        private Passenger paula;
 
-	    @BeforeEach
-	    void setUp() {
-	    	vueloFuerteventura = new Flight("FV001", 100);
-	    	vueloLanzarote = new Flight("LZ001", 82);
-	    	laura = new Passenger("id-PS01-1","Laura", "ES");
-	    	eduardo = new Passenger("id-PS02-1","Eduardo", "GE");
-	    	paula = new Passenger("id-PS02-2","Paula", "US");
-	    }
+        @BeforeEach
+        void setUp() {
+            vueloFuerteventura = new Flight("FV001", 100);
+            vueloLanzarote = new Flight("LZ001", 82);
+            laura = new Passenger("id-PS01-1", "Laura", "ES");
+            eduardo = new Passenger("id-PS02-1", "Eduardo", "GE");
+            paula = new Passenger("id-PS02-2", "Paula", "US");
+        }
 
-	        @Test
-	        @DisplayName("Comprobación de atributos")
-	        void testParametersPassenger() {
+        @Test
+        @DisplayName("Comprobación de atributos")
+        void testParametersPassenger() {
 
-	        	laura.setFlight(vueloLanzarote);
+            laura.setFlight(vueloLanzarote);
 
-	            assertAll("Verifica todas las condiciones un intercambio a través de joinFlight",
-	                    () -> assertEquals("Miguel", laura.getName()),
-	                    () -> assertEquals("GE", eduardo.getCountryCode()),
-	                    () -> assertEquals(null, paula.getFlight()),
-	                    () -> {
-	                    	paula.setFlight(vueloFuerteventura);
-	                    	assertEquals("FV001", paula.getFlight().getFlightNumber());
-	                    },
-	            );
-	        }
-	}
+            assertAll("Verifica todas las condiciones un intercambio a través de joinFlight",
+                    () -> assertEquals("Laura", laura.getName()), // Fix: Corrected name to "Laura"
+                    () -> assertEquals("GE", eduardo.getCountryCode()),
+                    () -> assertEquals(null, paula.getFlight()),
+                    () -> {
+                        paula.setFlight(vueloFuerteventura);
+                        assertEquals("FV001", paula.getFlight().getFlightNumber());
+                    }
+            );
+        }
+    }
 }
